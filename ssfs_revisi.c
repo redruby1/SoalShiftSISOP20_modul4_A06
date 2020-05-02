94ac56463be9b8b26bd6c35ca7c694e5f37f40d0
A06account <61569610+A06account@users.noreply.github.com>
2020-05-02 19:50:45 +0700

    Update ssfs_revisi.c

--- ssfs_revisi.c
+++ ssfs_revisi.c
@@ -1,28 +1,24 @@
 #define FUSE_USE_VERSION 28
-
 #include <fuse.h>
-
 #include <stdio.h>
-
 #include <string.h>
-
 #include <unistd.h>
-
 #include <fcntl.h>
-
 #include <dirent.h>
-
 #include <errno.h>
-
 #include <sys/time.h>
+#include <pwd.h>
+#include <grp.h>
 
-char str[100];
+static  const  char *dirpath = "/mnt/c/Users/Anisa\ Aurafitri/Documents";
+
+char str[1000];
 int i, x;
 
-char message[100] = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO";
+char message[1000] = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO";
 int key = 10, titik;
 
-char *enkripsi(char code[100]) {
+char *enkripsi(char code[1000]) {
 	
 	for(i = 0; (i < strlen(code) && code[i] != '\0'); i++) {
 		if(code[i] != '/') {
@@ -34,7 +30,7 @@ char *enkripsi(char code[100]) {
 	return code;
 }
 
-char *dekripsi(char code[100]) {
+char *dekripsi(char code[1000]) {
 	
 	for(i = 0; (i < strlen(code) && code[i] != '\0'); i++) {
 		if(code[i] != '/') {
@@ -46,170 +42,115 @@ char *dekripsi(char code[100]) {
 	return code;
 }
 
-char *get_eks(char code[100]) {
+char *get_eks(char code[1000]) {
 	const char *dot = strrchr(code, '.');
     if(!dot || dot == code) return "";
     return dot;
 }
 
-static int xmp_mkdir(const char *path, mode_t x)
-{
-    int res;
-    
-    res = mkdir(path, x);
-    if(res == -1)
-    {
-      return -errno;
-    }
-    
-    return 0;
+static  int  xmp_getattr(const char *path, struct stat *stbuf) {
+	int res;
+	char lokasi[1000];
+	char fpath[1000];
+
+	sprintf(lokasi, "%s", path);
+	enkripsi(lokasi);
+
+	sprintf(fpath,"%s%s",dirpath,lokasi);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
 }
 
-static int xmp_rename(const char *awal, const char *akhir)
-{
-     int res;
-     
-     res = rename(awal, akhir);
-     if(res == -1)
-     {
-      return -errno;
-     }
-     
-     return 0;
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, 
+			off_t offset, struct fuse_file_info *fi) {
+	
+	char lokasi[1000];
+	char fpath[1000];
+
+	sprintf(lokasi, "%s", path);
+	enkripsi(lokasi);
+	
+	if(strcmp(path,"/") == 0) {
+		path = dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else {
+    	sprintf(fpath, "%s%s", dirpath, lokasi);
+	}
+	
+	DIR *dp;
+	struct dirent *de;
+	
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	
+	if(dp == NULL)
+		return -errno;
+
+	while((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		
+		if(strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+			if(filler(buf, de->d_name, &st, 0))
+				break;
+		}
+	}
+
+	closedir(dp);
+	return 0;
 }
-
-static  int  xmp_getattr(const char *path, struct stat *stbuf)
-
-{
-
-int res;
-
-res = lstat(path, stbuf);
-
-if (res == -1)
-
-return -errno;
-
-  
-
-return 0;
-
-}
-
-  
-
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-
-off_t offset, struct fuse_file_info *fi)
-
-{
-
-DIR *dp;
-
-struct dirent *de;
-
-  
-
-(void) offset;
-
-(void) fi;
-
-  
-
-dp = opendir(path);
-
-if (dp == NULL)
-
-return -errno;
-
-  
-
-while ((de = readdir(dp)) != NULL) {
-
-struct stat st;
-
-memset(&st, 0, sizeof(st));
-
-st.st_ino = de->d_ino;
-
-st.st_mode = de->d_type << 12;
-
-if (filler(buf, de->d_name, &st, 0))
-
-break;
-
-}
-
-  
-
-closedir(dp);
-
-return 0;
-
-}
-
-  
 
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+			struct fuse_file_info *fi) {
 
-struct fuse_file_info *fi)
-
-{
-
-int fd;
-
-int res;
-
-  
-
-(void) fi;
-
-fd = open(path, O_RDONLY);
-
-if (fd == -1)
-
-return -errno;
-
-  
-
-res = pread(fd, buf, size, offset);
-
-if (res == -1)
-
-res = -errno;
-
-  
-
-close(fd);
-
-return res;
-
+	char lokasi[1000];
+	char fpath[1000];
+	
+	sprintf(lokasi, "%s", path);
+	enkripsi(lokasi);
+	
+	if(strcmp(path,"/") == 0) {
+		path = dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else {
+		sprintf(fpath, "%s%s",dirpath,path);
+	}
+	
+	int res = 0;
+	int fd = 0 ;
+	
+	(void) fi;
+	
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+	
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+	
+	close(fd);
+	return res;
 }
 
-  
-
 static struct fuse_operations xmp_oper = {
-
-.getattr = xmp_getattr,
-
-.readdir = xmp_readdir,
-
-.read = xmp_read,
-
-.mkdir = xmp_mkdir,
-
-.rename = xmp_rename
-
+	.getattr = xmp_getattr,
+	.readdir = xmp_readdir,
+	.read = xmp_read,
 };
 
-  
-
-int  main(int  argc, char *argv[])
-
-{
-
-umask(0);
-
-return fuse_main(argc, argv, &xmp_oper, NULL);
-
+int  main(int  argc, char *argv[]) {
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
